d9c04a6aa93c50f5ad03e76df87a2cdd21a5a712
Mark Enright <enright.mark@gmail.com>
2019-08-16 19:12:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
     list.each do |holiday, list|
       temp_holiday = holiday.to_s.capitalize
       temp_list = list.join(', ')
-      
+      temp_list.length.times 
       
      
       puts "  #{temp_holiday}: #{temp_list}"
