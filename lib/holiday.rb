4e1b4de6e995706c09c7e7609de0939df51dd7b4
Mark Enright <enright.mark@gmail.com>
2019-08-16 19:58:35 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,30 +18,41 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_supplies[:summer][:fourth_of_july][1]
+  
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].each do |holiday, items|
+    items.append(supply)
+  end
+holiday_hash
 end
+
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring].each do |holiday, items|
+    items.append(supply)
+  end
+holiday_hash
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten
 
 end
 
@@ -53,15 +64,37 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, list|
+    puts season.to_s.capitalize + ":"
+    list.each do |holiday, list|
+      temp_holiday = holiday.to_s
+      
+      temp_holiday = temp_holiday.split("_").map(&:capitalize).join(' ')
+      
 
+
+
+       temp_list = list.join(", ")    
+      puts "  #{temp_holiday}: #{temp_list}"
+      
+    end
+  end
+    
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  new_arr = []
+  holiday_hash.each do |season, data|
+    data.each do |holiday, stuff|
+      if stuff.include?("BBQ")
+        new_arr.append(holiday)
+      end
+    end
+  end
+  new_arr
 end
-
 
 
 
